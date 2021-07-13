8dd83e7076f11928b408585f3fe559b4b4e96c66
李兆龙 <62770320+Bruse-Lee@users.noreply.github.com>
2021-07-14 01:05:37 +0800

    删除冗余代码

--- surveryfront/src/components/sidebar/sidebar.vue
+++ surveryfront/src/components/sidebar/sidebar.vue
@@ -8,50 +8,6 @@
     router
   >
     <sidebarItem :menus="menus"></sidebarItem>
-    <!-- <el-submenu>
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>主页导航</span>
-      </template>
-      <el-menu-item-group>
-        <el-menu-item index="/home" route="/home">首页</el-menu-item>
-        <el-menu-item class="el-icon-edit" index="/log" route="/log">
-          日志管理</el-menu-item
-        >
-        <el-menu-item class="el-icon-user" index="profile" route="/profile">
-          个人信息</el-menu-item
-        >
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <template slot="title">权限管理</template>
-        <el-submenu index="1-5">
-          <template slot="title">用户管理</template>
-          <ul>
-            <el-menu-item index="userList" route="userList">
-              <i class="el-icon-menu"></i>
-              <span slot="title">用户列表</span>
-            </el-menu-item>
-            <el-menu-item index="1-7">
-              <i class="el-icon-menu"></i>
-              <span slot="title">添加用户</span>
-            </el-menu-item>
-          </ul>
-        </el-submenu>
-      </el-submenu>
-    </el-submenu>
-    <el-submenu index="1-5">
-      <template slot="title">个人中心</template>
-      <ul>
-        <el-menu-item index="1-6">
-          <i class="el-icon-menu"></i>
-          <span slot="title">个人信息</span>
-        </el-menu-item>
-        <el-menu-item index="1-7">
-          <i class="el-icon-menu"></i>
-          <span slot="title">密码更改</span>
-        </el-menu-item>
-      </ul>
-    </el-submenu> -->
   </el-menu>
 </template>
 
@@ -61,7 +17,6 @@ export default {
   components: {
     sidebarItem
   },
-  // name: "sidebarItem",
   props: {
     menus: {
       type: Array,
--- surveryfront/src/components/sidebar/sidebarItem.vue
+++ surveryfront/src/components/sidebar/sidebarItem.vue
@@ -11,22 +11,6 @@
         </template>
 
         <sidebar-item :menus="menu.children"></sidebar-item>
-
-        <!-- <el-menu-item-group v-for="child in menu.children" :key="child.path">
-          <el-menu-item>{{ child.title }}</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu v-for="menu in menus" :key="menu.path">
-          <template slot="title">{{ menu.title }}</template>
-          <el-submenu>
-            <template slot="title">{{ child.title }}</template>
-            <ul>
-              <el-menu-item>
-                <i class="el-icon-menu"></i>
-                <span slot="title">{{ child.title }}</span>
-              </el-menu-item>
-            </ul>
-          </el-submenu>
-        </el-submenu> -->
       </el-submenu>
 
       <el-menu-item v-else :index="menu.path">
