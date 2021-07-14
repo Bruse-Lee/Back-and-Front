c0fcaa85b10c82f213f5a14d6c9cd08594b51d0e
李兆龙 <62770320+Bruse-Lee@users.noreply.github.com>
2021-07-14 16:57:11 +0800

    添加axios访问URL的注释

--- surveryfront/src/components/sidebar/home.vue
+++ surveryfront/src/components/sidebar/home.vue
@@ -1,32 +1,4 @@
 <template>
-  <!-- <div>
-    <el-main>
-      <el-table :data="tableData" style="width: 100%" max-height="250">
-        <el-table-column fixed prop="id" label="ID" width="150">
-        </el-table-column>
-        <el-table-column prop="username" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="password" label="密码" width="120">
-        </el-table-column>
-        <el-table-column prop="createTime" label="创建时间" width="300">
-        </el-table-column>
-        <el-table-column prop="updateTime" label="更新时间" width="120">
-        </el-table-column>
-
-        <el-table-column fixed="right" label="操作" width="120">
-          <template slot-scope="scope">
-            <el-button
-              @click.native.prevent="deleteRow(scope.$index, tableData)"
-              type="text"
-              size="small"
-            >
-              移除
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </div> -->
   <div>
     <el-table :data="tableData" height="800px" border style="width: 100%">
       <el-table-column fixed prop="id" label="ID" width="150"></el-table-column>
@@ -57,9 +29,9 @@
 <script>
 export default {
   created: function () {
-    this.$axios.get("http://localhost:5000/user").then((res) => {
+    this.$axios.get("/user").then((res) => {
       this.tableData = res.data.data;
-      console.log(res.data.data);
+      console.log(res);
     });
   },
   data() {
--- surveryfront/src/main.js
+++ surveryfront/src/main.js
@@ -5,6 +5,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 
+// 指定axios访问的URL
+axios.defaults.baseURL = 'http://localhost:5000'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
