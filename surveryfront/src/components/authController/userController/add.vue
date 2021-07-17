beaf29f24cfdc2cf706692e9f0ef873f4f9294a8
李兆龙 <62770320+Bruse-Lee@users.noreply.github.com>
2021-07-17 10:56:00 +0800

    保存模板进行添加的最后工作

---
+++ surveryfront/src/components/authController/userController/add.vue
@@ -0,0 +1,169 @@
+<template>
+    <div>
+        <el-dialog :title="title" :visible.sync="addUserVisible"  width="55%"
+                   :before-close="handleCancel" :close-on-click-modal="false" @open="openAddUser()">
+            <el-form label-position="right" label-width="100px" :inline="true" :model="userInfo" :rules="rules" ref="user"
+                     style="padding: 0px 70px 0px 10px;" class="demo-ruleForm">
+                <el-form-item label="用户名" prop="userName">
+                    <el-input v-model="userInfo.userName" @blur="checkUserName" clearable placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input type="password" v-model="userInfo.password" placeholder="请输入登录密码"
+                              autocomplete="off" clearable></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="onSubmit('user')">创建用户</el-button>
+                <el-button @click="resetForm('user')">重置信息</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AddUser",
+        props: {
+            addUserVisible: Boolean,
+            data: [Object, Boolean],
+            title: String,
+        },
+        data() {
+            var validatePass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入密码'));
+                } else {
+                    if (this.userInfo.checkPassword !== '') {
+                        this.$refs.user.validateField('checkPassword');
+                    }
+                    callback();
+                }
+            };
+            // var validatePass2 = (rule, value, callback) => {
+            //     if (value === '') {
+            //         callback(new Error('请再次输入密码'));
+            //     } else if (value !== this.userInfo.password) {
+            //         callback(new Error('两次输入密码不一致!'));
+            //     } else {
+            //         callback();
+            //     }
+            // };
+            // var checktel = (rule, value, callback) => {
+            //     if (!value || value === "") {
+            //         return callback(new Error('手机号码不能为空'));
+            //     }
+            //     if (!(/^1[3|4|5|7|8|9][0-9]\d{4,8}$/.test(value))) {
+            //         callback(new Error('请输入合法的手机号码'));
+            //     } else {
+            //         callback();
+            //     }
+            // };
+            // var checkidentity = (rule, value, callback) => {
+            //     if (!value || value === "") {
+            //         return callback(new Error('身份证号不能为空'));
+            //     }
+            //     if (!(/^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/.test(value))) {
+            //         callback(new Error('请输入合法的身份证号码'));
+            //     } else {
+            //         callback();
+            //     }
+            // };
+            return {
+                userInfo: {
+                    userId: '',
+                    userName: '',
+                    // realName: '',
+                    // identity: '',
+                    // sex: '1',
+                    // address: '',
+                    // phone: '',
+                    // position: '',
+                    // roleId: '1',
+                    // roleName: '',
+                    // userStatus: '1',
+                    password: '',
+                    // checkPassword: ''
+                },
+                rules: {
+                    userName: [
+                        {required: true, message: '请输入用户名', trigger: 'blur'},
+                        {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, validator: validatePass, trigger: 'blur'}
+                    ],
+                    // checkPassword: [
+                    //     {required: true, validator: validatePass2, trigger: 'blur'}
+                    // ],
+                    // realName: [
+                    //     {required: true, message: '请输入真实名字', trigger: 'blur'},
+                    //     {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur'}
+                    // ],
+                    // identity: [
+                    //     {required: true, validator: checkidentity, trigger: 'blur'}
+                    // ],
+                    // address: [
+                    //     {required: true, message: '请输入家庭地址', trigger: 'blur'}
+                    // ],
+                    // phone: [
+                    //     {required: true, validator: checktel, trigger: 'blur'},
+                    // ],
+                    // position: [
+                    //     {required: true, message: '请输入职业称谓', trigger: 'blur'}
+                    // ]
+                }
+            }
+        },
+        methods: {
+            openAddUser() {
+                this.$nextTick(function () {
+                    this.$refs['user'].resetFields();
+                    for (var key in this.entity) {
+                        if (this.entity.hasOwnProperty.call(key)) {
+                            this.entity[key] = '';
+                        }
+                    }
+                });
+            },
+            //关闭弹窗
+            handleCancel() {
+                this.$refs['user'].resetFields();
+                this.$emit('cancel');
+            },
+            onSubmit(formName) {
+                let formData = this.userInfo;
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.$emit('val-change', formData);
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                })
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+            checkUserName(){
+                let app = this;
+                let data={
+                    userName:app.userInfo.userName
+                }
+                app.$Api.checkUserName(data, function (result) {
+                    if (result.result == "false") {
+                        app.$notify({
+                            title: '温馨提示：',
+                            message: result.message,
+                            type: 'warning'
+                        });
+                        app.userInfo.userName='';
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- surveryfront/src/components/authController/userController/userList.vue
+++ surveryfront/src/components/authController/userController/userList.vue
@@ -1,115 +1,34 @@
 <template>
   <el-table
-    :data="tableData"
-    max-height="770px"
-    border
-    class="elTable"
-    highlight-current-row
+    :data="
+      tableData.filter(
+        (data) =>
+          !search || data.name.toLowerCase().includes(search.toLowerCase())
+      )
+    "
+    style="width: 100%"
   >
-    <el-table-column
-      fixed
-      prop="id"
-      label="ID"
-      width="100"
-      align="center"
-    ></el-table-column>
-    <el-table-column
-      prop="username"
-      label="姓名"
-      width="350"
-      align="center"
-    ></el-table-column>
-    <el-table-column
-      prop="password"
-      label="密码"
-      width="300"
-      align="center"
-    ></el-table-column>
-    <el-table-column
-      prop="createTime"
-      label="创建时间"
-      width="300"
-      align="center"
-    ></el-table-column>
-    <el-table-column
-      prop="updateTime"
-      label="更新时间"
-      width="300"
-      align="center"
-    ></el-table-column>
-    <el-table-column label="操作" align="center">
-      <template slot-scope="scope">
-        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-          >编辑</el-button
-        >
-        <el-button
-          size="mini"
-          type="primary"
-          @click="handleAdd(scope.$index, scope.row.id)"
-          >添加</el-button
-        >
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row.id)"
-          >删除</el-button
-        >
+    <el-table-column align="left" >
+      <template slot="header">
+        <el-input class="search" v-model="search" size="mini" placeholder="输入关键字搜索" />
       </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
-import { GetList, deleteById } from "../../../api/user";
 export default {
-  mounted: function () {
-    GetList().then((res) => {
-      this.tableData = res.data.data;
-      console.log(res.data.data);
-    });
-  },
   data() {
     return {
-      tableData: null,
+      tableData: [],
+      search: "",
     };
-  },
-  // 注入reload方法
-  // 这样就实现了子组件调取reload方法就实现了刷新vue组件的功能,这样应该是它实现了组件跨越组件传递数据方法。
-  inject: ["reload"],
-  methods: {
-    handleEdit(index, row) {
-      console.log(index, row);
-    },
-    async handleDelete(index, id) {
-      const confirmResult = await this.$confirm(
-        "此操作将永久删除该用户,是否继续？",
-        "提示",
-        {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-        }
-      ).catch((err) => err);
-      if (confirmResult === "confirm") {
-        this.$message.success("删除成功!");
-        this.tableData.splice(index, 1);
-        deleteById(id);
-        // this.reload()  // 调用的刷新页面方法
-      } else {
-        this.$message.info("取消删除!");
-      }
-      console.log(id);
-    },
   },
 };
 </script>
 
 <style scoped>
-.elTable {
-  border-radius: 10px;
+.search{
+  width: 200px;
 }
-</style>>
-
-
-
-
+</style>
